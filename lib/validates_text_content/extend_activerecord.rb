0a7d153c3a60b281fed642a2e3102365cc9b05cd
Aaron Gough <aaron@aarongough.com>
2010-04-12 02:25:52 +0000

    Added test for examining how the validation deals with being supplied a nil or empty value Changed validation code to deal correctly with nil or empty values

--- lib/validates_text_content/extend_activerecord.rb
+++ lib/validates_text_content/extend_activerecord.rb
@@ -6,21 +6,25 @@ module ActiveRecord
         config = { :message => " - Correct capitalization and punctuation are required. (ie: All sentences should begin with a capital letter and end with a punctuation mark, etc.)"}
         configuration.update(attr_names.last) if attr_names.last.is_a?(Hash)
         validates_each attr_names do |model, attr, value|
-          error = false
-          # The text should start with a capital letter
-          error = true if( value.match(/[A-Z]/).nil? )
-          
-          # The text should contain at least one punctuation mark
-          error = true if( value.match(/[\.?!]/).nil? )
-          
-          # There should be at least one 'e' per 30 characters
-          error = true if( value.downcase.count('e') < (value.downcase.gsub(/[^a-z]*/, '').length / 30) )
-          
-          # There should be at least one space per 20 characters
-          error = true if( value.downcase.count(' ') < (value.length / 20) )
-          
-          # The text should be at least 25% lowercase
-          error = true if( value.gsub(/[^a-z]*/, '').length < (value.downcase.gsub(/[^a-z]*/, '').length / 4) )
+          if( value.nil? || value.length == 0 )
+            error = true
+          else
+            error = false
+            # The text should start with a capital letter
+            error = true if( value.match(/[A-Z]/).nil? )
+            
+            # The text should contain at least one punctuation mark
+            error = true if( value.match(/[\.?!]/).nil? )
+            
+            # There should be at least one 'e' per 30 characters
+            error = true if( value.downcase.count('e') < (value.downcase.gsub(/[^a-z]*/, '').length / 30) )
+            
+            # There should be at least one space per 20 characters
+            error = true if( value.downcase.count(' ') < (value.length / 20) )
+            
+            # The text should be at least 25% lowercase
+            error = true if( value.gsub(/[^a-z]*/, '').length < (value.downcase.gsub(/[^a-z]*/, '').length / 4) )
+          end
           
           model.errors.add( attr, config[:message] ) if( error )
         end
--- test/validates_text_content_test.rb
+++ test/validates_text_content_test.rb
@@ -20,15 +20,19 @@ class ValidatesTextContentTest < Test::Unit::TestCase
     assert Article.new(@sample_article).save
   end
   
+  
   def test_items_with_junk_text_should_be_rejected
-    junk_text = []
-    junk_text << @sample_book.merge({ :content => 'this comment has absolutely no capitalization.' })
-    junk_text << @sample_book.merge({ :content => 'This comment has no type of punctuation' })
-    junk_text << @sample_book.merge({ :content => "Fdkjd dk dkkjhd kjdhkjd kjdhdhdl alkasla lka alk." })
-    junk_text << @sample_book.merge({ :content => "THIS COMMENT IS ALL IN UPPERCASE. ME NO WANT." })
-    junk_text << @sample_book.merge({ :content => "Thiscommenthasabsolutelynospacesthereisnowaythisisgood." })
-    junk_text << @sample_book.merge({ :content => "THIS COMMENT is MORE THAN 80% CAPITALS." })
-    junk_text << @sample_book.merge({ :content => "THIS pERSON hAS tHEIR cAPS-lOCK oN." })
+    junk_text = [
+      @sample_book.merge({ :content => nil }),
+      @sample_book.merge({ :content => "" }),
+      @sample_book.merge({ :content => 'this comment has absolutely no capitalization.' }),
+      @sample_book.merge({ :content => 'This comment has no type of punctuation' }),
+      @sample_book.merge({ :content => "Fdkjd dk dkkjhd kjdhkjd kjdhdhdl alkasla lka alk." }),
+      @sample_book.merge({ :content => "THIS COMMENT IS ALL IN UPPERCASE. ME NO WANT." }),
+      @sample_book.merge({ :content => "Thiscommenthasabsolutelynospacesthereisnowaythisisgood." }),
+      @sample_book.merge({ :content => "THIS COMMENT is MORE THAN 80% CAPITALS." }),
+      @sample_book.merge({ :content => "THIS pERSON hAS tHEIR cAPS-lOCK oN." })
+    ]
     junk_text.each do |junk_text_item|
       @expected_book_count = Book.count
       assert_equal false, Book.new( junk_text_item ).save, "Text rejection failed on: #{junk_text_item.to_a.join(" : ")}"
@@ -37,14 +41,15 @@ class ValidatesTextContentTest < Test::Unit::TestCase
   end
   
   def test_comments_with_good_content_should_be_created
-    good_text = []
-    good_text << @sample_book.merge({ :content => 'This is a very simple test comment.' })
-    good_text << @sample_book.merge({ :content => 'Supercalifragilisticexpialadocious: a super long word!' })
-    good_text << @sample_book.merge({ :content => 'How many words can I think of that don\'t contain an "e"?' })
-    good_text << @sample_book.merge({ :content => 'WHAT?! You mean to tell *ME* that SONY/ERICSSON *CAN\'T* make a phone?' })
-    good_text << @sample_book.merge({ :content => 'WHAT?! YOU MEAN TO TELL ME that SONY/ERICSSON *CAN\'T* make a phone?' })
-    good_text << @sample_book.merge({ :content => 'ROFL! Who do I have to shag to get a comment around here?' })
-    good_text << @sample_book.merge({ :content => 'I\'m a Klingon. It\'s kind of my thing to be angry for almost no good reason. Yesterday I tore the door off my fridge cause I was out of margarine. These things happen.' })
+    good_text = [
+      @sample_book.merge({ :content => 'This is a very simple test comment.' }),
+      @sample_book.merge({ :content => 'Supercalifragilisticexpialadocious: a super long word!' }),
+      @sample_book.merge({ :content => 'How many words can I think of that don\'t contain an "e"?' }),
+      @sample_book.merge({ :content => 'WHAT?! You mean to tell *ME* that SONY/ERICSSON *CAN\'T* make a phone?' }),
+      @sample_book.merge({ :content => 'WHAT?! YOU MEAN TO TELL ME that SONY/ERICSSON *CAN\'T* make a phone?' }),
+      @sample_book.merge({ :content => 'ROFL! Who do I have to shag to get a comment around here?' }),
+      @sample_book.merge({ :content => 'I\'m a Klingon. It\'s kind of my thing to be angry for almost no good reason. Yesterday I tore the door off my fridge cause I was out of margarine. These things happen.' })
+    ]
     good_text.each do |good_text_item|
       @expected_book_count = Book.count + 1
       assert Book.new( good_text_item ).save, "Text creation failed on: #{good_text_item.to_a.join(" : ")}"
